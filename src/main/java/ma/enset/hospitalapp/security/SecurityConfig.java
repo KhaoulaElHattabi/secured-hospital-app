6f9b9224dbb623e7bbd38b381a364f0e50cb7513
KhaoulaElHattabi <khaoulaelhattabi00@gmail.com>
2023-04-23 20:38:32 +0100

    Fix bugs

--- src/main/java/ma/enset/hospitalapp/security/SecurityConfig.java
+++ src/main/java/ma/enset/hospitalapp/security/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
@@ -15,6 +16,12 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Autowired
     private PasswordEncoder passwordEncoder;
+
+   // @Bean
+    public UserDetailsService userDetailsService(String usern){
+        return null;
+    }
+
     @Bean
     public InMemoryUserDetailsManager inMemoryUserDetailsManager(){
         return new InMemoryUserDetailsManager(
